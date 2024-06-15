8a3f6f56f74947c55c4322f61f3c22f2fe1453fc
VladoLov <vladislav.lovric@gmail.com>
2024-06-15 15:37:57 +0200

    serv

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -266,7 +266,7 @@ export async function createBooking(newBooking) {
 // UPDATE
 
 // The updatedFields is an object which should ONLY contain the updated data
-export async function updateGuest(id, updatedFields) {
+/* export async function updateGuest(id, updatedFields) {
   const { data, error } = await supabase
     .from("guests")
     .update(updatedFields)
@@ -308,3 +308,4 @@ export async function deleteBooking(id) {
   }
   return data;
 }
+ */
